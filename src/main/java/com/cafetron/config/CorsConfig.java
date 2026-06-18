d54e7ccc9e08afeb8200a0860edf1e5d06080f91
Dhruv S Shah <dhruvsshah171@gmail.com>
2026-06-18 16:33:48 +0530

    cors

--- src/main/java/com/cafetron/config/CorsConfig.java
+++ src/main/java/com/cafetron/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOriginPatterns(parseAllowedOriginPatterns());
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
